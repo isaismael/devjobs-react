9884f851f90e023c87d263922e17b18285d2958f
Ismael Isa <ismael.isa02@gmail.com>
2023-04-15 15:54:01 -0300

    se agrego mapeado de data.jsx en joblist

--- src/routers/routes.jsx
+++ src/routers/routes.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Header } from "../components/Header";
 import { JobLists } from "../components/JobLists";
+import { JobDetails } from "../components/JobDetails";
 
 export const MyRoutes = () => {
   return (
     <Router>
         <Routes>
             <Route exact path='/' element={<JobLists />} />
-            <Route exact path='/jobs' element={<JobDetails />} />
-            <Route exact path='/jobs:position' element={<JobDetails />} />
+            <Route exact path='/jobs' element={<JobLists />} />
+            <Route exact path='/jobs/:position' element={<JobDetails />} />
         </Routes>
     </Router>
   )
