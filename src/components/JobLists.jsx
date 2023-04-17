914f248c66805b4ee116fd5675fb0bd0cb5bdaf5
Ismael Isa <ismael.isa02@gmail.com>
2023-04-17 20:04:19 -0300

    se agrego los search para filtrar busquedas e imagenes

--- src/components/JobLists.jsx
+++ src/components/JobLists.jsx
@@ -5,16 +5,32 @@ import jobs from "../data/data";
 export const JobLists = () => {
   const [jobData, setJobData] = useState(jobs);
 
-  const [searchTerm, setSearchtTerm] = useState('');
+  const [searchTerm, setSearchtTerm] = useState("");
 
-  const [searchByLocation, setSearchByLocation] = useState('');
+  const [searchByLocation, setSearchByLocation] = useState("");
 
   const searchTermValue = searchTerm.toLowerCase();
 
-  const locationSearchHandler =  () => {
-    const filteredData = jobs.filter((job) => job.location.toLowerCase().includes(searchByLocation.toLowerCase()))
-    setJobData(filteredData)
-  }
+  const locationSearchHandler = () => {
+    const filteredData = jobs.filter((job) =>
+      job.location.toLowerCase().includes(searchByLocation.toLowerCase())
+    );
+    setJobData(filteredData);
+  };
+
+  const filterJobData = (e) => {
+    const filterValue = e.target.value;
+    if (filterValue === "full-time") {
+      const filterdData = jobs.filter((job) => job.contract === "Full Time");
+      setJobData(filterdData);
+    } else if (filterValue === "part-time") {
+      const filterdData = jobs.filter((job) => job.contract === "Part Time");
+      setJobData(filterdData);
+    } else if (filterValue === "freelance") {
+      const filterdData = jobs.filter((job) => job.contract === "Freelance");
+      setJobData(filterdData);
+    }
+  };
 
   return (
     <section className="job__list">
@@ -30,13 +46,21 @@ export const JobLists = () => {
               />
             </div>
             <div className="search__panel-02">
-            <input
+              <input
                 type="text"
                 placeholder="Búsqueda por Ubicación"
                 value={searchByLocation}
                 onChange={(e) => setSearchByLocation(e.target.value)}
               />
               <button onClick={locationSearchHandler}>Buscar</button>
+            </div>
+            <div className="search__panel-03">
+              <select onChange={filterJobData}>
+                <option>Filtrar trabajo por:</option>
+                <option value="full-time">Full - Time</option>
+                <option value="part-time">Part - Time</option>
+                <option value='freelance'>Freelance</option>
+              </select>
             </div>
           </div>
           <div className="job__wrapper">
@@ -51,6 +75,7 @@ export const JobLists = () => {
               })
               .map((item) => (
                 <div className="job__item" key={item.id}>
+                  <img src={item.logo} alt="" />
                   <div className="job__content">
                     <h6>
                       {item.postedAt} - {item.contract}
